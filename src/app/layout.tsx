880eb0bb815c91b9e5686c751555eeffdff70a28
Elephant24456 <rlaxortn0984@naver.com>
2024-07-04 09:01:26 +0900

    layout 완성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="bg-black font-bold text-xl text-center text-white py-4 px-4">
+          포켓몬 도감
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
